5e22c85f305a53f2635be8d1a58303ada8f1e4a0
samuelmalik <samuelmalik9@gmail.com>
2022-10-08 21:44:14 +0200

    added function returning centuries

--- Open-Lab-05.01/DateTools.cs
+++ Open-Lab-05.01/DateTools.cs
@@ -6,7 +6,27 @@ namespace Open_Lab_05._01
     {
         public string Century(int year)
         {
-            throw new NotImplementedException();
+            double year_double = year / 100.0;
+            string century = Math.Ceiling(year_double).ToString();
+            Console.WriteLine(century);
+            if (century != "11" && century != "12" && century != "13")
+            {
+            switch (century[century.Length-1])
+            {
+                case '1':
+                    return century + "st century";
+                case '2':
+                    return century + "nd century";
+                case '3':
+                    return century + "rd century";
+                default:
+                    return century + "th century";
+            }
+            }
+            else
+            {
+                return century + "th century";
+            }
         }
     }
 }
